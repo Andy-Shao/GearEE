8b637be428f481f8844f387d92119b9020348ba0
Andy-Shao <hj13035952619@126.com>
2015-12-29 12:07:49 +0800

    adding testing model

--- src/test/java/com/github/andyshaox/servlet/mapping/MappingTestingClass.java
+++ src/test/java/com/github/andyshaox/servlet/mapping/MappingTestingClass.java
@@ -4,6 +4,7 @@ import javax.servlet.http.HttpServletRequest;
 
 import com.github.andyshaox.servlet.mapping.annotation.Attribute;
 import com.github.andyshaox.servlet.mapping.annotation.Mapping;
+import com.github.andyshaox.servlet.mapping.annotation.PathVariable;
 
 @Mapping("/mapping")
 public class MappingTestingClass {
@@ -21,14 +22,19 @@ public class MappingTestingClass {
     public String requestProcess(String username) {
         return "/testingOne.html";
     }
-    
+
     @Mapping("/process2")
-    public String requestProcess2(@Attribute("password")String password){
+    public String requestProcess2(@Attribute("password") String password) {
         return "/testingTwo.html";
     }
-    
+
     @Mapping
-    public String requestProcess3(@Attribute String username, HttpServletRequest request){
+    public String requestProcess3(@Attribute String username , HttpServletRequest request) {
         return "/testingThree.html";
+    }
+
+    @Mapping("/process4/{username}/{passwd}")
+    public String requestProcess4(String username , @PathVariable("passwd") String password) {
+        return "/testingFour.html";
     }
 }
